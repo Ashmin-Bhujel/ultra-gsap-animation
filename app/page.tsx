0a5bfe0ce1912e1aa0635ac8a6f1f2f189129b12
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-07-15 18:29:28 +0545

    feat: integrate prettier and GSAP

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
-      <h1 className="font-bold text-center text-4xl">Ultra GSAP Animation</h1>
+      <h1 className="text-center text-4xl font-bold">Ultra GSAP Animation</h1>
     </main>
   );
 }
